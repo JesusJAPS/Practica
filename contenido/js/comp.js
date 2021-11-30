19a82903e9e4c07570e6540ba6bbd78821a52c92
Jesus <jesusp5568@gmail.com>
2021-11-30 09:42:22 -0600

    Primer commit

---
+++ contenido/js/comp.js
@@ -0,0 +1,57 @@
+let prince = document.querySelector(".prince img");
+
+prince.addEventListener('mouseover', mensaje);
+
+function mensaje() {
+    alert("Es una pelicula del 2010 dirigida por Mike Newell.");
+}
+
+let fast = document.querySelector(".fast img");
+
+fast.addEventListener('mouseover', mostrar);
+
+function mostrar() {
+    alert("The Fast and the Furious: Tokyo Drift es una pelicula estadounidense, dirigida por Justin Lin y protagonizada por Lucas Black, Bow Wow, Sung Kang, Nathalie Kelley y Brian Tee. Es la tercera pelicula de la saga The Fast and the Furious.");
+}
+
+let transformers = document.querySelector(".transformers img");
+
+transformers.addEventListener('mouseover', most);
+
+function most() {
+    alert("Transformers es una pelicula de accion y ciencia ficcion comercial estadounidense de 2007 fue dirigida por Michael Bay, con Steven Spielberg como productor ejecutivo.");
+}
+
+let Tierra = document.querySelector(".Tierra img");
+
+Tierra.addEventListener('mouseover', mens);
+
+function mens() {
+    alert("After Earth (Despues de la Tierra en Hispanoamerica) es una pelicula de ciencia ficción y aventuras dirigida por M. Night Shyamalan y protagonizada por Jaden Smith y Will Smith en los papeles principales.");
+}
+
+let rocky = document.querySelector(".rocky img");
+
+rocky.addEventListener('mouseover', tex);
+
+function tex() {
+    alert("Rocky es una pelicula estadounidense de 1976 escrita y protagonizada por Sylvester Stallone y dirigida por John G. Avildsen.");
+}
+
+let leyenda = document.querySelector(".leyenda img");
+
+leyenda.addEventListener('mouseover', mens);
+
+function mens() {
+    alert("Soy leyenda (en ingles: I Am Legend) es una pelocula de terror y ciencia ficcion postapocaliptica estadounidense de 2007, dirigida por Francis Lawrence y protagonizada por Will Smith.");
+}
+
+//let eliminarbtn = document.querySelector('.img');
+
+//eliminarbtn.addEventListener('mouseover', eliminar);
+
+//function eliminar() {
+    //alert('Elimino un boton');
+    //let imag = document.querySelector('.img');
+    //imag.src = 'img/inter.jpg';
+//}
